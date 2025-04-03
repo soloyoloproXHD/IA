9ae5584ff72e8065744795dc82d042bb1fae92e9
Hiram Cornejo <hiramcornejo10@gmail.com>
2025-04-03 00:41:27 -0600

    Esta cosa medio detecta las emociones xD
    Ayuda por favor me quiero matar no me salia nadota @-@

--- Actividades/puntosFaciales/Documentacion.md
+++ Actividades/puntosFaciales/Documentacion.md
@@ -5,27 +5,28 @@
 
 # Modelar un proceso para la detección de vida
 
-## ¿Cuales son los parametros que tomaria para poder identificar que lo que se está viendo es alguien vivo (gestos, emociones, acciones) y no una imagen?
+## ¿Cuáles son los parametros que tomaria para poder identificar que lo que se está viendo es alguien vivo (gestos, emociones, acciones) y no una imagen?
 
 ### Parametros a considerar:
->- Coordenada de los punto en el area facial dependiendo de la          emoción o accción.
->- Distancia entre dos o más puntos de varias zonas caracteristicas como lo son las sejas, barbilla y boca.
->- Cambios de iluminación frame x frame.
+>- Coordenada relativa de los punto en el area facial determinada dependiendo de la emoción o accción.
+>- Distancia entre dos o más puntos (con las coordenadas relativas) de varias zonas caracteristicas como lo son las sejas, parpados, labios, bordes de la boca, etc.
     
-## Cuantos puntos son requeridos?
-> Esto depende de cuales zonas del rostro se tomarán en cuenta. Por ejemplo, algúnas de las zonas mas relevantes a tomar en cuenta son las siguientes:
+## ¿Cuántos puntos son requeridos?
+> <div align="justify"> Esto depende de cuales zonas del rostro se tomarán en cuenta. Por ejemplo, algúnas de las zonas mas relevantes a tomar en cuenta son las siguientes: </div>
+>
 > - Sejas
-> - Boca
-> - Barbilla
-> - Ojos
+> - Boca (labios y bordes)
+> - Mejillas
+> - Ojos (parpados)
 
 ## ¿Cuales son los puntos que mas información nos dan?
 > Clasificando por zona los puntos que mas información pueden proporcionar son los siguientes:
-> - Boca (La forma de la boca cambia con las emociones como felicidad, enojo, sorpresa):
->   - Labios
-> - Ojos:
-Al igual que la boca, los ojos tienen variacion en su forma, en este caso es en la apertura de los ojos, variando la apertura de estos (más especificamente los parpados), estos cambios son muy notorios con la felicidad, la sorpresa y la fatiga.
->   - Parpados
->   - Pupilas
+> - Boca (La forma de la boca cambia con las emociones como felicidad, enojo, tristeza, sorpresa):
+> - Ojos (Los parpados son un punto importante a tomar en cuenta.)
+> - Sejas
 
-Cual sería una de las mejores estrategias para que un sistema de reconocimiento facial no sea violado?
+> <div align="justify">Al igual que la boca, los ojos tienen variacion en su forma, en este caso es en la apertura de los ojos, variando la apertura de estos (más especificamente los parpados), estos cambios son muy notorios con la felicidad, la sorpresa y la fatiga.<div>
+>   - Parpados
+>   - Pupilas (Estas pueden ser usadas para determinar si es algo vivo lo que se está capturando)
+
+## ¿Cual sería una de las mejores estrategias para que un sistema de reconocimiento facial no sea violado?
--- Actividades/puntosFaciales/puntosFaciales.py
+++ Actividades/puntosFaciales/puntosFaciales.py
@@ -10,8 +10,8 @@ face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=2,
 # Captura de video
 cap = cv2.VideoCapture(0)
 
-# Lista de índices de landmarks específicos (ojos, boca, cejas y nariz)
-selected_points = [33, 133, 362, 263, 61, 291, 70, 105, 107, 336, 334, 300, 4, 48, 278, 8]  # Ojos, boca, cejas y nariz
+# Lista de índices de landmarks específicos
+selected_points = [33, 133, 362, 263, 61, 291, 70, 105, 107, 336, 334, 300, 4, 48, 278, 8, 374, 386, 159, 145, 13, 14, 50, 280]
 
 def distancia(p1, p2):
     """Calcula la distancia euclidiana entre dos puntos."""
@@ -45,26 +45,98 @@ while cap.isOpened():
                 d_ceja_der = distancia(puntos[300], puntos[336])
                 d_nariz = distancia(puntos[4], puntos[8])
                 
-                # Calcular proporciones
-                proporcion_ojos_boca = (d_ojo_izq + d_ojo_der) / (2 * d_boca)
-                proporcion_cejas_nariz = (d_ceja_izq + d_ceja_der) / (2 * d_nariz)
-                
-                # Determinar simetría
-                simetria = "Simetrica" if abs(d_ojo_izq - d_ojo_der) < 10 and abs(d_ceja_izq - d_ceja_der) < 10 else "No Simetrica"
-                
-                # Mostrar proporciones y simetría en el marco
-                cv2.putText(frame, f"Proporcion Ojos-Boca: {proporcion_ojos_boca:.2f}", (10, 30), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-                cv2.putText(frame, f"Proporcion Cejas-Nariz: {proporcion_cejas_nariz:.2f}", (10, 60), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-                cv2.putText(frame, f"Simetria: {simetria}", (10, 90), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-                
                 # Dibujar líneas entre puntos clave
-                cv2.line(frame, puntos[33], puntos[133], (23, 255, 23), 2)  # Ojo izquierdo
-                cv2.line(frame, puntos[362], puntos[263], (23, 255, 23), 2)  # Ojo derecho
-                cv2.line(frame, puntos[61], puntos[291], (23, 255, 23), 2)  # Boca
                 cv2.line(frame, puntos[4], puntos[8], (0, 255, 255), 2)  # Nariz
+
+                # Obtener la posición del punto de la nariz (índice 4)
+                if 4 in puntos:
+                    x_nariz, y_nariz = puntos[4]
+                    # Mostrar la posición en el marco
+                    cv2.putText(frame, f"Nariz: ({x_nariz}, {y_nariz})", (10, 120), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+                if 13 and 14 and 61 and 291 and 107 and 336 and 133 and 362 in puntos:
+                    # Encontrar los límites del área facial
+                    x_min = min(p[0] for p in puntos.values())
+                    x_max = max(p[0] for p in puntos.values())
+                    y_min = min(p[1] for p in puntos.values())
+                    y_max = max(p[1] for p in puntos.values())
+                    
+                    # área facial
+                    ancho_facial = x_max - x_min
+                    alto_facial = y_max - y_min
+                    
+                    # Coordenadas absolutas de los puntos
+                    x_boca_izq, y_boca_izq = puntos[61]
+                    x_boca_der, y_boca_der = puntos[291]
+                    
+                    x_labio_sup, y_labio_sup = puntos[13]
+                    x_labio_inf, y_labio_inf = puntos[14]
+                    
+                    x_seja_izq, y_seja_izq = puntos[107]
+                    x_seja_der, y_seja_der = puntos[336]
+                    
+                    x_ojo_izq, y_ojo_izq = puntos[133]
+                    x_ojo_der, y_ojo_der = puntos[362]
+
+                    
+                    # Coordenadas relativas de los puntos de la boca
+                    x_relativo_boca_izq = (x_boca_izq - x_min) / ancho_facial
+                    y_relativo_boca_izq = (y_boca_izq - y_min) / alto_facial
+                    x_relativo_boca_der = (x_boca_der - x_min) / ancho_facial
+                    y_relativo_boca_der = (y_boca_der - y_min) / alto_facial
+                    
+                    x_relativo_labio_sup = (x_labio_sup - x_min) / ancho_facial
+                    y_relativo_labio_sup = (y_labio_sup - y_min) / alto_facial
+                    x_relativo_labio_inf = (x_labio_inf - x_min) / ancho_facial
+                    y_relativo_labio_inf = (y_labio_inf - y_min) / alto_facial
+                    
+                    x_relativo_seja_izq = (x_seja_izq - x_min) / ancho_facial
+                    y_relativo_seja_izq = (y_seja_izq - y_min) / alto_facial
+                    x_relativo_seja_der = (x_seja_der - x_min) / ancho_facial
+                    y_relativo_seja_der = (y_seja_der - y_min) / alto_facial
+                    
+                    x_relativo_ojo_izq = (x_ojo_izq - x_min) / ancho_facial
+                    y_relativo_ojo_izq = (y_ojo_izq - y_min) / alto_facial
+                    x_relativo_ojo_der = (x_ojo_der - x_min) / ancho_facial
+                    y_relativo_ojo_der = (y_ojo_der - y_min) / alto_facial
+                    
+                    
+                    
+                    # Calcular distancias horizontal entre los puntos de la boca
+                    distancia_horizontal_boca = abs(x_relativo_boca_der - x_relativo_boca_izq)
+                    distancia_vertical_boca = abs(y_relativo_labio_sup - y_relativo_labio_inf) #Se calcula respecto de los labios
+                    distancia_seja_ojo_izq = abs(y_relativo_seja_izq - y_relativo_ojo_izq)
+                    distancia_seja_ojo_der = abs(y_relativo_seja_der - y_relativo_ojo_der)
+
+                    promedio_seja_ojo = (distancia_seja_ojo_izq + distancia_seja_ojo_der) / 2
+                    
+                    # Depuración: Imprimir valores
+                    print(f"Distancia Horizontal Boca: {distancia_horizontal_boca}")
+                    print(f"Distancia Vertical Boca: {distancia_vertical_boca}")
+                    print(f"Distancia Seja Ojo Izq: {distancia_seja_ojo_izq}")
+                    print(f"Distancia Seja Ojo Der: {distancia_seja_ojo_der}")
+                    print(f"Promedio Seja Ojo: {promedio_seja_ojo}")
+
+                    # Clasificar la expresión facial
+                    if distancia_horizontal_boca > 0.50:  # Umbral para sonrisa
+                        emocion = "Feliz"
+                    elif distancia_horizontal_boca < 0.45 and promedio_seja_ojo > 0.24:  # Umbral para tristeza
+                        emocion = "Triste"
+                    elif distancia_vertical_boca > 0.10 and promedio_seja_ojo > 0.24:  # Umbral para sorpresa
+                        emocion = "Sorpresa"
+                    elif distancia_vertical_boca < 0.45 and promedio_seja_ojo < 0.24:  # Umbral para enojo
+                        emocion = "Enojo"
+                    else:
+                        emocion = "Neutral"
+                    
+                    cv2.putText(frame, f"Emocion: {emocion}", (10, 240), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    
+                    cv2.putText(frame, f"Boca Iz Relativa: ({x_relativo_boca_izq:.2f}, {y_relativo_boca_izq:.2f})", (10, 180),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+                    cv2.putText(frame, f"Boca Der Relativa: ({x_relativo_boca_der:.2f}, {y_relativo_boca_der:.2f})", (10, 210),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
     cv2.imshow('PuntosFacialesMediaPipe', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
